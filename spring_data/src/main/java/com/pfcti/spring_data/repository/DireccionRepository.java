9caf32db9b0412b1d2f99db3a624043271178a4d
Max Acevedo Retana <max.acevedo@pfcti.com>
2023-03-19 14:15:32 -0600

    Practica

---
+++ spring_data/src/main/java/com/pfcti/spring_data/repository/DireccionRepository.java
@@ -0,0 +1,8 @@
+package com.pfcti.springdata.repository;
+
+import com.pfcti.springdata.model.Cuenta;
+import com.pfcti.springdata.model.Direccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DireccionRepository extends JpaRepository<Direccion, Integer> {
+}
